e7e9d0546dd2fb9dea7fa46fe79cd43cd5e73440
Ramin Khatibi <rkhatibi@twitter.com>
2016-10-24 17:07:36 -0700

    update gems to 3.8.7

--- puppet.rb
+++ puppet.rb
@@ -2,7 +2,7 @@ class PuppetGem < FPM::Cookery::Recipe
   description 'Puppet gem stack'
 
   name 'puppet'
-  version '3.7.3'
+  version '3.8.7'
 
   source "nothing", :with => :noop
 
@@ -18,10 +18,11 @@ class PuppetGem < FPM::Cookery::Recipe
 
   def build
     # Install gems using the gem command from destdir
-    gem_install 'facter',      '2.3.0'
+    gem_install 'facter',      '2.4.6'
     gem_install 'json_pure',   '1.8.1'
     gem_install 'hiera',       '1.3.4'
-    gem_install 'deep_merge',  '1.0.1'
+    gem_install 'deep_merge',  '1.1.1'
+    gem_install 'rack',        '1.6.4'
     gem_install 'rgen',        '0.7.0'
     gem_install 'ruby-augeas', '0.5.0'
     gem_install 'ruby-shadow', '2.3.4'
@@ -33,8 +34,9 @@ class PuppetGem < FPM::Cookery::Recipe
   end
 
   def install
-    # Install init-script and puppet.conf
-    install_files
+    # don't install the init-script and puppet.conf
+    # into the local OS
+    #install_files
 
     # Provide 'safe' binaries in /opt/<package>/bin like Vagrant does
     rm_rf "#{destdir}/../bin"
